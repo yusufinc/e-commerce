d30adf19fdb81726428fd2fcbb04919a51b9854a
Yusuf Incesu <yusufandscad@gmail.com>
2023-08-07 13:40:39 +0300

    the contact page has been edited and I have done the validation, Requests

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -2,28 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ContentFormRequest;
 use App\Models\Contact;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class AjaxController extends Controller
 {
-    public function iletisimkaydet(Request $request){
+    public function iletisimkaydet(ContentFormRequest $request){
         // $data = $request->all();
         // $data['ip'] = $request->ip();
 
 
+   /* $validationdata = $request->validate([
+            'name'=>'required|string|min:2',
+            'email' => 'required|email',
+            'subject'=> 'required',
+            'message'=> 'required'
+    ],[
+        'name.required' => 'isim soyisim girmeniz gerekiyor',
+        'name.string' => 'isim soyisim karakterden oluşmalı',
+        'name.min' => 'İsim soyisim en az 2 karakterli olabilir',
+
+
+        'email.required' => 'email girmeniz gerekiyor',
+        'email.email' => 'email formatında girmeniz gerekiyor',
+
+
+        'subject.required' => 'konu girmeniz gerekiyor',
+        'message.required' => 'Mesajınızı girmeniz gerekiyor',
+
+    ]); */
         $newData = [
             'name'=>Str::title($request->name),
             'email'=>$request->email,
-            'email'=>$request->subject,
-            'email'=>$request->message,
+            'subject'=>$request->subject,
+            'message'=>$request->message,
             'ip'=>request()->ip(),
 
 
         ];
       $sonkaydedilen =  Contact::create($newData);
 
-      return back()->withSuccess('Başarıyla gönderildi');
+      return back()->with(['message'=>'Başarıyla gönderildi']);
     }
 }
