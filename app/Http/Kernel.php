f64bb5fa8a80b27b87b83e3b29fbb45bd796bbff
Yusuf Incesu <yusufandscad@gmail.com>
2023-08-10 15:30:44 +0300

    admin panel page created

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,6 +66,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'sitesetting' => \App\Http\Middleware\SiteSettingMiddleware::class
+        'sitesetting' => \App\Http\Middleware\SiteSettingMiddleware::class,
+        'panelsetting' => \App\Http\Middleware\PanelSettingMiddleware::class
     ];
 }
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -34,6 +34,9 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->group(base_path('routes/panel.php'));
+
+                Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
     }
