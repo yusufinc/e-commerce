128a2cbbd41d996ec2239f858415bd6f2a372245
Yusuf Incesu <yusufandscad@gmail.com>
2023-08-11 16:35:00 +0300

    make panel slider

--- routes/panel.php
+++ routes/panel.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\Backend\DashboardController;
+use App\Http\Controllers\Backend\SliderController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::group(['middleware'=>['panelsetting','auth'],'prefix'=>'panel','as'=>'panel.'], function() {
 
     Route::get('/',[DashboardController::class,'index'])->name('panel');
+
+    Route::get('/slider',[SliderController::class,'index'])->name('slider');
 
 });
